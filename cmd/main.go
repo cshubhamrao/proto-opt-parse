7d805e46297f2c702d0807f8233587064f132f74
Shubham Rao <shubham@nimblebox.ai>
2024-10-10 20:15:09 +0530

    also parse comments

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,32 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 
 	greeterv1 "github.com/cshubhamrao/proto-opt-parse/gen/greeter/v1"
 	optsv1 "github.com/cshubhamrao/proto-opt-parse/gen/opts/v1"
+	"google.golang.org/protobuf/encoding/prototext"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/descriptorpb"
 )
 
 var log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 
 func main() {
 	log.Debug("Starting")
+
+	protoFds, err := loadProtoDescriptors("./desc.txtpb")
+	if err != nil {
+		log.Error("Failed to load proto descriptors", "error", err)
+		os.Exit(1)
+	}
+
 	protoFd := greeterv1.File_greeter_v1_hello_proto // get the file descriptor
 	svcs := protoFd.Services()                       // get list of services defined
+
 	for i := 0; i < svcs.Len(); i++ {
 		svc := svcs.Get(i) // Get the service
 		svcName := svc.FullName()
@@ -25,6 +39,12 @@ func main() {
 		for i := 0; i < methods.Len(); i++ {
 			method := methods.Get(i) // Get the method
 			fmt.Printf("\tMETHOD --> %s\n", method.FullName().Name())
+
+			// Extract and print the method comment
+			comment := getComment(protoFds, method)
+			if comment != "" {
+				fmt.Printf("\t\tCOMMENT --> %s\n", comment)
+			}
 
 			options := method.Options()                              // Get the options for the method
 			if proto.HasExtension(options, optsv1.E_LoggingConfig) { // Check if our extension is present
@@ -41,4 +61,27 @@ func main() {
 			}
 		}
 	}
+}
+
+func getComment(protoFds *protoregistry.Files, method protoreflect.Descriptor) string {
+	md, err := protoFds.FindDescriptorByName(method.FullName())
+	if err != nil {
+		fmt.Println("Error finding descriptor")
+		return ""
+	}
+	return strings.TrimSpace(md.ParentFile().SourceLocations().ByDescriptor(md).LeadingComments)
+}
+
+func loadProtoDescriptors(filePath string) (*protoregistry.Files, error) {
+	descriptorBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read descriptor file: %w", err)
+	}
+
+	fileDescriptorSet := &descriptorpb.FileDescriptorSet{}
+	if err := prototext.Unmarshal(descriptorBytes, fileDescriptorSet); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal descriptor: %w", err)
+	}
+
+	return protodesc.NewFiles(fileDescriptorSet)
 }
